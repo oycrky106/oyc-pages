ab6f6fee6bef7c226062008b7cb7ef4722ce77e4
oycrky106 <sedfqtukjyt@hotmail.com>
2023-08-19 12:02:20 +0800

    bugfix

--- test/index.test.js
+++
@@ -1,8 +0,0 @@
-const oycPages = require('..')
-
-// TODO: Implement module test
-test('oyc-pages', () => {
-  expect(oycPages('w')).toBe('w@zce.me')
-  expect(oycPages('w', { host: 'wedn.net' })).toBe('w@wedn.net')
-  expect(() => oycPages(100)).toThrow('Expected a string, got number')
-})
